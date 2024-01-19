7bf3292c444fc324a8c97fc85be8ee00a94e3d15
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2024-01-19 16:52:26 +0500

    in process

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,5 +1,45 @@
 import React from "react";
+import { Box, Grid, Typography, TextField, Checkbox } from "@mui/material";
+import styled from "styled-components";
+
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
+const CheckboxDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  /* justify-content: center; */
+`;
 
 export default function Form() {
-  return <div>Form</div>;
+  return (
+    <Box>
+      <Grid
+        container
+        sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+      >
+        <Grid item xs={8}>
+          <Typography component="h5" variant="h5">
+            Form Demo
+          </Typography>
+          <TextField
+            id="outlined-basic"
+            label="Text Input"
+            fullWidth
+            variant="outlined"
+          />
+          <Typography
+            component="h6"
+            variant="h6"
+            sx={{ color: "gray", fontSize: "14px" }}
+          >
+            Radio Input
+          </Typography>
+          <CheckboxDiv>
+            <Checkbox label="Radion Option 1" />
+            <Checkbox label="Radion Option 1" />
+          </CheckboxDiv>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
